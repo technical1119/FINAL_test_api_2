8e58d0e3f7d9233ba97a7e606bc6d80328fc124e
technical1119 <o.galych@prolife.de>
2024-12-28 19:14:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,6 @@ def get_project_details_endpoint(request: URLRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    import uvicorn
+    port = int(os.environ.get("PORT", 6000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
